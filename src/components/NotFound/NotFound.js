17f71d198097b2fde3aaeed5061683da98283a86
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-04-02 15:25:27 +0530

    dev: bugfix: removed notfound.css

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import NotFoundLogo from "../../assets/404.svg";
-import "./NotFound.css";
 
 const NotFound = () => {
 	return (

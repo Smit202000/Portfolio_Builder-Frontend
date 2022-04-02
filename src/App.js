a06c0f28ef01a37e14de00167c3bf8687b10b3f3
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-04-02 15:20:22 +0530

    dev: configured basic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthPage, Login, NotFound, Signup } from "./components";
 import "./App.css";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Navigate to={"/login"} />} />
-			<Route path="/login" element={<p>Login</p>} />
-			<Route path="/signup" element={<p>Signup</p>} />
+			<Route path="/auth" element={<AuthPage />}>
+				<Route path="/auth/login" element={<Login />} />
+				<Route path="/auth/signup" element={<Signup />} />
+			</Route>
 			<Route path="/portfolio/:username" element={<p>portfolio here</p>} />
-			<Route path="*" element={<p>404 not found</p>} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }

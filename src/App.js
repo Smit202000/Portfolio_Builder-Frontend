a99bdff962f24c13ab63a817c2a685555df9e9fa
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-04-02 11:49:31 +0530

    dev: added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<p>srishti is a good girl</p>
-		</div>
+		<Routes>
+			<Route path="/" element={<Navigate to={"/login"} />} />
+			<Route path="/login" element={<p>Login</p>} />
+			<Route path="/signup" element={<p>Signup</p>} />
+			<Route path="/portfolio/:username" element={<p>portfolio here</p>} />
+			<Route path="*" element={<p>404 not found</p>} />
+		</Routes>
 	);
 }
 

14fbb5321458f914527f87ef1a19a99aa2e5cf23
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-04-02 15:23:22 +0530

    dev: exported all components

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,8 @@ export { default as ExampleComponent } from "./ExampleComponent/ExampleComponent
 export { default as TemplateOne } from "./templates/TemplateOne/TemplateOne";
 export { default as TemplateTwo } from "./templates/TemplateTwo/TemplateTwo";
 export { default as TemplateThree } from "./templates/TemplateThree/TemplateThree";
+export { default as NotFound } from "./NotFound/NotFound";
+export { default as AuthPage } from "./AuthPage/AuthPage";
+export { default as Login } from "./Login/Login";
+export { default as Signup } from "./Signup/Signup";
+export { default as HeroIntroSection } from "./HeroIntroSection/HeroIntroSection";

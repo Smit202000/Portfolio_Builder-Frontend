f7237f07ca2cdb8998ace90d18f1339222a51787
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-04-03 11:08:41 +0530

    dev: modified: PrivateRoute

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-// import { getToken } from "../../utils/tokens";
+import { getToken } from "../../utils/tokens";
 
 const PrivateRoute = ({ children }) => {
-	// const isAuthenticated = getToken() ? true : false;
-	const isAuthenticated = true;
-
+	const isAuthenticated =
+		getToken() !== "" &&
+		getToken() !== null &&
+		getToken() !== undefined &&
+		getToken() !== "undefined"
+			? true
+			: false;
 	return isAuthenticated ? children : <Navigate to="/auth/login" />;
 };
 

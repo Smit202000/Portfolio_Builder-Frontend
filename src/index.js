684d8187888b09249396fbedd743689a1c84e949
adarshpandya-simform <adarsh.p@simformsolutions.com>
2022-04-03 10:46:53 +0530

    dev: added: SnackbarProvider Wrapper

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { SnackbarProvider } from "notistack";
 import "./index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<Router>
-				<App />
-			</Router>
-		</Provider>
+		<SnackbarProvider autoHideDuration={3000} maxSnack={3}>
+			<Provider store={store}>
+				<Router>
+					<App />
+				</Router>
+			</Provider>
+		</SnackbarProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
